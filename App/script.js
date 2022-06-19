4a70d4a5393497eeb76f4643edb6ed8690228b72
wadocode <art.wado@gmail.com>
2022-06-19 16:32:11 -0500

    front ivezza al 90%

---
+++ App/script.js
@@ -0,0 +1,42 @@
+let menu=document.querySelector('#menu-btn')
+let navbar=document.querySelector('.header .navbar')
+
+menu.onclick= () =>{
+ menu.classList.toggle('fa-times');
+ navbar.classList.toggle('active');
+}
+
+menu.onscroll= () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+   }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop:true,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  breakpoints:{
+      0:{
+          slidesPerView:1,
+      },
+      600:{
+          slidesPerView:2,
+      },
+  },
+});
